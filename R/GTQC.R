7cfa34ef2061e49aca5bb60d24928a657bf6a3e7
ShilinZhao <zhaoshilin@gmail.com>
2020-01-25 01:21:58 -0600

    other package updates

--- R/GTQC.R
+++ R/GTQC.R
@@ -5,6 +5,7 @@
 #' @import tools
 #' @import data.table
 #' @import snpStats
+#' @import BSgenome.Hsapiens.UCSC.hg19
 usethis::use_package("data.table","Depends")
 usethis::use_package("maftools","Depends")
 usethis::use_package("htmltools","Depends")
@@ -13,3 +14,5 @@ usethis::use_package("knitr","Depends")
 usethis::use_package("rmarkdown","Depends")
 usethis::use_package("ggplot2","Depends")
 usethis::use_package("RColorBrewer","Depends")
+usethis::use_package("BiocStyle","Suggests")
+
